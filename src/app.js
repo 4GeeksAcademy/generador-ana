573eaa6ed537d2da9a925825157df047e580770b
anagit2025 <pintorosbelys90@gmail.com>
2025-04-25 17:56:11 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+
+window.onload = function () {
+    //write your code here
+    console.log("Hello Rigo from the console!");
+
+
+    let who = ['me levante con el pie izquierdo', 'amaneci lento hoy', 'mañana sera un mejor dia', 'hoy tengo hambre'];
+    let action = ['que dia es hoy', 'no me quiero bañar', 'un dia a la vez', 'no quiero trabajar'];
+    let what = ['tengo flojera', 'hoy es festivo', 'busca un error'];
+    let when = ['yo no se mañana', 'hellou', 'tengo hambre', 'no quiero cocinar', 'me duele la cabeza'];
+
+    console.log(who.concat(action, what, when))
+    console.log(action)
+    console.log(what)
+    console.log(when)
+
+    let whoRandom = Math.floor(Math.random() * who.length);
+    let actionRandom = Math.floor(Math.random() * action.length);
+    let whatRandom = Math.floor(Math.random() * what.length);
+    let whenRandom = Math.floor(Math.random() * when.length);
+
+    document.getElementById("theexcuse").innerHTML = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom]
 };
+
